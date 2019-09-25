21351bd0565443f2c4838d6b866f0a9c5bc9cae9
Pan Ziyue <random.rrr3@gmail.com>
2019-09-25 23:01:39 +0800

    suppressed unzip messages

--- docs/library_install.sh
+++ docs/library_install.sh
@@ -2,7 +2,7 @@
 
 USER=$(whoami)
 
-DIR="/Users/$USER/Documents/Arduino/libraries/SSTuino_Companion"
+DIR="/Users/$USER/Documents/Arduino/libraries/SSTuino_Companion-master"
 LIB_DIR="/Users/$USER/Documents/Arduino/libraries/"
 DL_DIR="/Users/$USER/Downloads"
 
@@ -14,6 +14,7 @@ fi
 echo "Downloading new version..."
 curl -L https://github.com/d3lta-v/SSTuino_Companion/archive/master.zip -o "$DL_DIR/SSTuino_Companion.zip"
 
-unzip "$DL_DIR/SSTuino_Companion.zip" -d "$LIB_DIR"
+echo "Extracting package..."
+unzip -qq "$DL_DIR/SSTuino_Companion.zip" -d "$LIB_DIR"
 
 echo "Installation complete!"
